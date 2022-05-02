6a77a2663289dbca1c3e357a2462c22b97b7bef1
Khai Truong <kminht@gmail.com>
2022-05-02 19:58:52 -0400

    vaild json data, write to file

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { animals } = require("./data/animals.json");
-
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 // parse incoming string or array data
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 // parse incoming json data
 app.use(express.json());
 
@@ -18,27 +19,44 @@ app.get("/api/animals", (req, res) => {
   res.json(results);
 });
 
-app.get('/api/animals/:id', (req,res) => {
-    const result = findById(req.params.id, animals);
-    if (result) {
-        res.json(result);
-    } else {
-        res.send(404);
-    }
+app.get("/api/animals/:id", (req, res) => {
+  const result = findById(req.params.id, animals);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404);
+  }
 });
 
-app.post('/api/animals', (req, res) => {
-    //req.body is where our incoming content will be
-    console.log(req.body);
-    res.json(req.body);
-})
+app.post("/api/animals", (req, res) => {
+  //set id based on what the next index of the array will be
+  req.body.id = animals.length.toString();
 
+  // if any data in req.body is incorrect, send 400 error back
+  if (!validateAnimal(req.body)) {
+    res.status(400).send("The animal is not properly formatted.");
+  } else {
+    // add animal to json file and animals array in this function
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+  }
+});
 
-function findById(id, animalsArray) {
-    const result = animalsArray.filter(animal => animal.id == id[0]);
-    return result;
+function createNewAnimal(body, animalsArray) {
+  const animal = body;
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname, "./data/animals.json"),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+  // return finished code to post route for response
+  return animal;
 }
 
+function findById(id, animalsArray) {
+  const result = animalsArray.filter((animal) => animal.id == id[0]);
+  return result;
+}
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -87,6 +105,23 @@ function filterByQuery(query, animalsArray) {
   }
   return filteredResults;
 }
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+      return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+      return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+      return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+      return false;
+    }
+    return true;
+  }
+  
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}`);
